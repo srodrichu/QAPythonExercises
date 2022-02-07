2d46add25cc3c9143362e0e0caacf3046b9978ad
srodrichu <sebastianawrchu@gmail.com>
2022-02-07 15:03:47 +0000

    SC: Add comment

--- pyexercise1.py
+++ pyexercise1.py
@@ -1,3 +1,5 @@
+#Create a python file that will input your first name, last name and outputs "Hello [firstname] [lastname]"
+
 firstName = input("Please enter your first name: ")
 secondName = input("Please enter your second name: ")
 
