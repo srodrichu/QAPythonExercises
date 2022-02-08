5249de972f6abc7a96d66a9d13896b5e9e272f5b
srodrichu <sebastianawrchu@gmail.com>
2022-02-08 10:52:07 +0000

    SC: pyexercise2 + solution

---
+++ pyexercises2.py
@@ -0,0 +1,15 @@
+#Task:
+#Write a program which will do the following:
+#Create a list which contains the first name of everyone in your cohort and store it in a variable called team.
+#Add your trainer to the list without manually editing it.
+#Print the list to the terminal.
+#Print only the 5th person in the list to your terminal
+
+team = ['Sebastian', 'Ahmed', 'Aidan', 'Baki','Ben','Daniel','Feruza','George','Hassan','Lelyzaveta','Ira','James','Santiago','Joseph','Juliana','Kenan','Leon','Lewis','Martyn','Nabil']
+
+team.append('Ryan')
+
+print(team)
+
+print(team[6])
+
