0ddfbdb3821b5657e126d955a9109f45e01525b0
srodrichu <sebastianawrchu@gmail.com>
2022-02-08 12:10:17 +0000

    SC: Conditional exercise

--- pyexercises2.py
+++ pyexercises2.py
@@ -13,3 +13,34 @@ print(team)
 
 print(team[4])
 
+##################
+
+#Task:
+#Create a new Python file and write a program that does the following:
+#Asks for an input from the user as a mark.
+#If the mark is greater that 85 print "Distinction"
+#If the mark is between 65 and 85 print "Pass"
+#Anything else print "Fail"
+
+#Assignment grade
+
+a_grade = float(input("What is your assignment grade out of 50?: "))
+
+#Exam grade
+
+e_grade = float(input("What is your exam grade out of 100?: "))
+
+#Practical grade
+
+p_grade = float(input("What is your practical grade out of 25?: "))
+
+#Average grade
+
+av_grade = (((a_grade / 50) + (e_grade / 100) + (p_grade / 25)) * 100) / 3
+
+if av_grade >= 80:
+    print('Distinction', av_grade)
+elif av_grade >= 65 and av_grade <= 80: 
+    print('Pass', av_grade)
+else:
+    print('Fail', av_grade)
